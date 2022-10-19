4ae429c87b746153ca8cfe6fc204f17408ef65d2
Saad Akllam <sakllam@e2r8p15.1337.ma>
2022-10-19 18:54:03 +0100

    ex01

---
+++ Module 00/ex01/Program.java
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Program.java                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 18:09:48 by sakllam           #+#    #+#             */
+/*   Updated: 2022/10/19 18:52:59 by sakllam          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.util.*;
+
+public class Program
+{
+    public static void main(String args[])
+    {
+        Scanner x = new Scanner(System.in);
+        int iternb = 1;
+        System.out.print("-> ");
+        try 
+        {
+            int i = x.nextInt();
+            if (i <= 0)
+            {
+                System.out.println("Illegal arg msg!");
+                return ;
+            }
+            for (int number = 2; number *number <= i; number++)
+            {
+                if (i % number == 0)    
+                {
+                    System.out.print("false ");
+                    System.out.println(iternb);
+                    return ;
+                }
+                iternb++;
+            }
+            System.out.print("true ");
+            System.out.println(iternb);
+        }
+        catch (InputMismatchException e)
+        {
+            System.out.println("Bro write a valid integer");
+        }
+    }
+}

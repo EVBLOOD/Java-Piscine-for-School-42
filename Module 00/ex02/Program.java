4f4c8fdff63d24febfabd333f406b7b1f7f856a4
Saad Akllam <sakllam@e2r8p15.1337.ma>
2022-10-19 19:19:48 +0100

    ex02

---
+++ Module 00/ex02/Program.java
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Program.java                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 18:56:34 by sakllam           #+#    #+#             */
+/*   Updated: 2022/10/19 19:19:03 by sakllam          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.util.*;
+
+public class Program
+{
+    private static int ft_sum(int i)
+    {
+        int sum = 0;
+        while (i != 0)
+        {
+            sum += i % 10;
+            i = i / 10;
+        }
+        return sum;
+    }
+    private static Boolean ft_isprime(int i)
+    {
+        for (int number = 2; number *number <= i; number++)
+        {
+            if (i % number == 0)    
+                return false;
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+
+        int number;
+        int total = 0;
+        while (true)
+        {
+            System.out.print("-> ");
+            number = in.nextInt();
+            if (number <= 0)
+            {
+                System.err.println("IlegalArgument");
+                return;
+            }
+            if (number == 42)
+                break;
+            if (ft_isprime(ft_sum(number)))
+                total++;
+        }
+        System.out.println("Count of coffee-request - " + total);
+    }
+}
